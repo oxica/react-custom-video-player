ffd56d3001bb4409374f3736ceb671a6ffb7cf2f
Oksana <kskaplun@gmail.com>
2022-05-30 22:49:40 +0300

    fix

--- src/components/ReactVideoPlayer.js
+++ src/components/ReactVideoPlayer.js
@@ -2,17 +2,13 @@ import React from 'react';
 import { DefaultPlayer as Video } from 'react-html5video';
 import 'react-html5video/dist/styles.css';
 
-import introVideo from '../videos/pexels.mp4';
-// import thum from '../img/thum.jpg';
+import introVideo from '../videos/colors.mp4';
+import thum from '../img/thum.jpg';
 
 const ReactVideoPlayer = () => {
   return (
     <Video autoPlay loop>
-      <source
-        src={introVideo}
-        type="video/webm"
-        poster="https://www.pexels.com/ru-ru/photo/3622474/"
-      />
+      <source src={introVideo} type="video/webm" poster={thum} />
     </Video>
   );
 };
