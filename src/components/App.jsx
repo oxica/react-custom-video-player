11949413118efb0583caf4ba5adf655adedb073e
Oksana <kskaplun@gmail.com>
2022-05-30 22:38:34 +0300

    add player

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import ReactVideoPlayer from './ReactVideoPlayer';
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    // style={{
+    //   height: '100vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 40,
+    //   color: '#010101',
+    // }}
     >
-      React homework template
+      <ReactVideoPlayer />
     </div>
   );
 };
